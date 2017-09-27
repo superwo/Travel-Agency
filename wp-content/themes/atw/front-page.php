bf180209c2c33b02e0ff5471afa99589616c573a
avadarad <avadanii@tiscali.it>
2017-09-27 11:09:26 +0300

    latest posts section

--- wp-content/themes/atw/front-page.php
+++ wp-content/themes/atw/front-page.php
@@ -1,5 +1,74 @@
 <?php get_header(); ?>
 
+  <section class="upcoming-tours">
+    <h2><span>Upcoming Travels</span></h2>
+    <?php
+      $args = array(
+        'post_type' => 'tour',
+        'orderby' => 'menu_order',
+        'order' => 'ASC',
+        'posts_per_page' => 3
+      );
+    ?>
+    <ul>
+      <?php $tours = new WP_Query($args); ?>
+      <?php while($tours->have_posts()): $tours->the_post(); ?>
+        <li class="grid1-3">
+          <div class="image">
+            <a href="">
+              <img class="more" src="<?php echo get_stylesheet_directory_uri(); ?>/img/moreinfo.png" alt="">
+            </a>
+            <?php my_responsive_thumbnail(get_the_ID()); ?>
+          </div>
 
+          <div class="content-tours">
+            <?php
+              // dates
+              $format = 'd F, Y';
+              $date = strtotime(get_field('leaving_date'));
+              $leavingDate = date_i18n($format, $date);
+
+              $returningDate = strtotime(get_field('returning_date'));
+              $returningDate = date_i18n($format, $returningDate);
+            ?>
+            <div class="date-price clear">
+              <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+              <p class="date"><?php echo $leavingDate . ' - ' . $returningDate ?></p>
+            </div>
+          </div>
+        </li>
+      <?php endwhile; wp_reset_postdata(); ?>
+    </ul>
+  </section>
+
+  <div class="bottom-content">
+    <section class="latest-entries grid2-3">
+      <h2><span>Travel Tips From our Blog</span></h2>
+
+      <ul>
+        <?php $args = array(
+          'post_type' => 'post',
+          'posts_per_page' => 2
+        );
+        $tips = new WP_Query($args);
+        while ($tips->have_posts()): $tips->the_post();
+        ?>
+          <li class="clear">
+            <div class="imagetips grid1-3">
+              <?php the_post_thumbnail('medium'); ?>
+            </div>
+            <div class="content-tip grid2-3 omega">
+              <h3><?php the_title(); ?></h3>
+              <?php html5wp_excerpt('html5wp_index'); ?>
+            </div>
+          </li>
+        <?php endwhile; wp_reset_postdata(); ?>
+      </ul>
+    </section>
+
+    <section class="latest-entries grid1-3">
+      <h2><span>Testimonials</span></h2>
+    </section>
+  </div>
 
 <?php get_footer(); ?>
--- wp-content/themes/atw/functions.php
+++ wp-content/themes/atw/functions.php
@@ -31,6 +31,38 @@ function slider() {
 }
 add_action('slider_index', 'slider');
 
+/**** RESPONSIVE IMAGES **/
+ /**
+ * Create function my_responsive_thumbnail() to output responsive featured image
+ * Function can be called from within the loop of any template file using
+ * my_responsive_thumbnail(get_the_ID());
+ */
+
+ function my_responsive_thumbnail($post_id){
+   // Get the featured image ID
+   $attachment_id = get_post_thumbnail_id($post_id);
+
+   // Get the info for each image size including the original (full)
+   $thumb_large    = wp_get_attachment_image_src($attachment_id, 'tourFront');
+   $thumb_medium   = wp_get_attachment_image_src($attachment_id, 'featuredTour');
+
+   // Create array containing each image size + the alt tag
+   $thumb_data = array(
+       'thumb_large'    => $thumb_large[0],
+       'thumb_medium'   => $thumb_medium[0],
+   );
+
+   // Echo out <picture> element based on code from above
+   echo '<picture>';
+   echo '<!--[if IE 9]><video style="display: none;"><![endif]-->'; // Fallback for IE9
+   echo '<source srcset="' . $thumb_data['thumb_large'] . ', ' . $thumb_data['thumb_large'] . ' " media="(min-width: 800px)">';
+   echo '<source srcset="' . $thumb_data['thumb_medium'] . ', ' . $thumb_data['thumb_medium'] . ' ">';
+   echo '<!--[if IE 9]></video><![endif]-->'; // Fallback for IE9
+   echo '<img srcset="' . $thumb_data['thumb_large'] . ', ' . $thumb_data['thumb_large'] . '">';
+   echo '</picture>';
+ }
+
+
 /*------------------------------------*\
 	Theme Support
 \*------------------------------------*/
@@ -52,6 +84,7 @@ if (function_exists('add_theme_support'))
     add_image_size( 'featuredTour', 730, 390, true );
     add_image_size( 'featuredBlog', 1280, 444, true );
     add_image_size( 'slider', 1500, 500, true );
+    add_image_size( 'tourFront', 430, 300, true );
 
     // Add Support for Custom Backgrounds - Uncomment below if you're going to use
     add_theme_support('custom-background', array(
@@ -125,6 +158,10 @@ function html5blank_header_scripts()
 
         wp_register_script('fancybox', get_template_directory_uri() . '/js/jquery.fancybox.js', array(), '2.1.7'); // Fancybox
         wp_enqueue_script('fancybox'); // Enqueue it!
+
+        wp_register_script('picturefill', get_template_directory_uri() . '/js/picturefill.js', array(), '3.0'); // picturefill
+        wp_enqueue_script('picturefill'); // Enqueue it!
+
 
         wp_register_script('html5blankscripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0'); // Custom scripts
         wp_enqueue_script('html5blankscripts'); // Enqueue it!
@@ -285,7 +322,7 @@ function html5wp_excerpt($length_callback = '', $more_callback = '')
 function html5_blank_view_article($more)
 {
     global $post;
-    return '... <a class="view-article" href="' . get_permalink($post->ID) . '">' . __('View Article', 'html5blank') . '</a>';
+    return '... <a class="view-article" href="' . get_permalink($post->ID) . '">' . __('Continue Reading', 'html5blank') . '</a>';
 }
 
 // Remove Admin bar
